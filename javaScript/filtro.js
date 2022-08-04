3054ca2376ff6b94a7f0b9d94f6f9bbb9985a3d4
Nelson <njpinget@gmail.com>
2022-08-04 16:36:09 -0300

    Se quito filtro. Creacion adaptacion movile en index

---
+++ javaScript/filtro.js
@@ -0,0 +1,18 @@
+
+/*const contenedor_g_l = document.getElementById('contenedor_g-l');
+
+const menu_logo = document.getElementById('menu-logo');
+
+const contenedor = document.getElementById('contenedor')
+const contenedor_producto = document.getElementById('contenedor-producto');
+
+let hijos = contenedor.childNodes;  //hijos
+console.log(hijos);
+
+
+function cambiar_menu(){
+    contenedor_g_l.classList.add('img_menu');
+    menu_logo.classList.add('')
+    hijos.classList.add('contenedor-producto_menu');
+
+}*/
