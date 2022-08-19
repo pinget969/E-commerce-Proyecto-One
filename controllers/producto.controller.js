948836382eed51afca81211a5879a4266cc68037
Nelson <njpinget@gmail.com>
2022-08-19 02:12:15 -0300

    imagen vista previa por url en agregar producto. creacion key-img localStorage. eliminacion de input file en administrador

--- controllers/producto.controller.js
+++ controllers/producto.controller.js
@@ -4,8 +4,9 @@ console.log(productosServices);
 //inicio cambiar clase
 let miClase1 = "btn_ocultar-eliminar"
 let miClase_editar = "btn_ocultar-editar"
-const crearNuevaLinea = (categoria, nombre, precio, id) =>{
-    console.log(categoria,"  ", nombre, "   ", precio)
+const crearNuevaLinea = (categoria, img_url, nombre, precio, id) =>{
+
+    console.log(categoria,"  ", nombre, "   ", precio, " url", img_url)
     if(localStorage.getItem('login')){
         const confirmar = JSON.parse(localStorage.getItem('login'));
         if(confirmar.logeado == true){
@@ -20,6 +21,7 @@ const crearNuevaLinea = (categoria, nombre, precio, id) =>{
     }
 
 //Fin cambiar clase
+
     const linea = document.createElement("div");
     const contenido = `<div class="contenedor">
     <div class="contenedor-producto" data-producto>
@@ -28,7 +30,7 @@ const crearNuevaLinea = (categoria, nombre, precio, id) =>{
         <button><a href="/editar.html?id=${id}"  class=${miClase_editar}>editar</a></button>
         </div>
         <div class="producto-ind">
-            <img id="img_foto" src="img/iconos-logos/picture_photo_image_icon_131252.png" alt="">
+            <img id="img_foto" src="${img_url}"  alt="">
             <p>Ver producto</p>
         </div>
         <div id="contenedor-nombre-producto" class="contenedor-nombre-producto"> 
@@ -59,7 +61,7 @@ const crearNuevaLinea = (categoria, nombre, precio, id) =>{
 //Recorriendo api
 productosServices.listaProductos().then((data) => {
     data.forEach((producto) => {
-        const nuevaLinea = crearNuevaLinea(producto.categoria, producto.nombre, producto.precio, producto.id);
+        const nuevaLinea = crearNuevaLinea(producto.categoria, producto.img_url, producto.nombre, producto.precio, producto.id);
         const contenidoNuevo = document.querySelector(`[data-${producto.categoria}]`) //asignacion de categoria
         contenidoNuevo.appendChild(nuevaLinea);
     });
--- javaScript/new_producto.js
+++ javaScript/new_producto.js
@@ -1,7 +1,8 @@
+/*Mantenimiento 1
 let vista_preliminar = (event)=>{
     let leer_img = new FileReader();
     let id_img = document.getElementById('img_foto');
-
+    const file = event.target.files[0];
     leer_img.onload = ()=>{
         if(leer_img.readyState==2){
             id_img.src = leer_img.result
@@ -9,12 +10,49 @@ let vista_preliminar = (event)=>{
     };
     leer_img.readAsDataURL(event.target.files[0]);
 };
+Mantenimiento. */
+
+const url_foto = document.getElementById("url_foto");
+
 const modal_container = document.getElementById('modal_container');
 function mostrar_nombre(valor){
     document.getElementById("contenedor-nombre-producto").innerHTML=valor;
 }
 function mostrar_precio(valor){
     document.getElementById("contenedor-precio-producto").innerHTML=valor;
+}
+
+const btn_url = document.getElementById('btn_url')
+
+const info_url = JSON.parse(localStorage.getItem('img'));
+
+const url_comp = info_url.cont_url // get final
+
+document.querySelector("[data-url]").value= url_comp;
+
+
+
+    const contenedor_img = document.createElement("img");
+    const contenido_img = `<img id="img_foto" src="${url_comp}" alt="">`;
+    contenedor_img.innerHTML=contenido_img;
+    document.getElementById("contenedor-img_uro-producto").innerHTML=contenido_img;
+
+btn_url.addEventListener("click", (evento)=>{
+    const url_foto = document.getElementById("url_foto");
+    const cont_url= url_foto.value
+    console.log(url_foto);
+    const imgAlm = {
+        cont_url,
+        };
+
+        localStorage.setItem('img', JSON.stringify(imgAlm));
+        window.location.href = "administrador.html";
+        
+});
+
+function mostrar_url(valor){
+    document.getElementById("").innerHTML=valor
+    console.log(valor)
 }
 
 function cerrar_admin(){
